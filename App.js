2e3b13e17a34440a8458b49b71122062b23c5711
Anshumant <anshumant@logic-square.com>
2020-11-22 17:32:27 +0530

    basic functionality

--- App.js
+++ App.js
@@ -1,12 +1,54 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, StatusBar, Platform, FlatList } from 'react-native';
+
+//components
+import Header from './components/header';
+import TodoItem from './components/todoItem';
+import AddToDo from './components/addToDo';
 
 export default function App() {
+  const [todos, setTodos] = useState([
+    { text: 'buy coffee', key: '1' },
+    { text: 'create an app', key: '2' },
+    { text: 'play on the switch', key: '3' }
+  ]);
+
+  const pressHandler = key => {
+    setTodos((prevTodos) => {
+      return prevTodos.filter(todo => {
+        return todo.key !== key
+      })
+    })
+  }
+
+  const submitHandler = (text) => {
+    setTodos((prevTodos) => {
+      return [
+        { text: text, key: Math.random().toString() },
+        ...prevTodos
+      ]
+    })
+  }
+
   return (
     <View style={styles.container}>
-      <Text>App Created</Text>
-      <StatusBar style="auto" />
+      <Header />
+      <View style={styles.content}>
+        <AddToDo
+          onSubmitHandler={submitHandler}
+        />
+        <View style={styles.list}>
+          <FlatList
+            data={todos}
+            renderItem={({ item }) => (
+              <TodoItem
+                item={item}
+                pressHandler={pressHandler}
+              />
+            )}
+          />
+        </View>
+      </View>
     </View>
   );
 }
@@ -15,7 +57,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : null
   },
+  content: {
+    padding: 40,
+  },
+  list: {
+    marginTop: 20
+  }
 });
---
+++ components/addToDo.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, TextInput, Button, View } from 'react-native';
+
+export default function AddToDo(props) {
+
+    const [text, setText] = useState('');
+
+    return (
+        <View>
+            <TextInput
+                placeholder='new todo..'
+                onChangeText={val => {
+                    setText(val);
+                    console.log(val);
+                }}
+            />
+            <Button
+                value={text}
+                onPress={() => {
+                    props.onSubmitHandler(text)
+                    setText((prev) => {
+                        prev = ''
+                        return prev
+                    })
+                }}
+                title='add todo'
+                color='coral'
+            />
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    input: {
+        marginBottom: 10,
+        paddingHorizontal: 8,
+        paddingVertical: 6,
+        borderBottomWidth: 1,
+        borderBottomColor: '#ddd'
+    }
+})
